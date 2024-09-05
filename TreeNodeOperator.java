981db073421d065654c794eb15aa901589e29fed
Bruno Antico Galin <74074230+BrunoAG77@users.noreply.github.com>
2024-09-05 19:26:27 -0300

    Update TreeNodeOperator.java

--- TreeNodeOperator.java
+++ TreeNodeOperator.java
@@ -1,3 +1,10 @@
+// Bruno Antico Galin 10417318
+// Ismael de Sousa e Silva 10410870
+// Referência: https://www.youtube.com/watch?v=Etpc_-br5rI
+// Referência: https://www.youtube.com/watch?v=b_NjndniOqY
+// Referência: https://www.youtube.com/watch?v=Gt2yBZAhsGM
+// Referência: https://www.youtube.com/watch?v=wL7JOLxbMI4
+
 package apl1_ed2;
 
 public class TreeNodeOperator extends TreeNode {
