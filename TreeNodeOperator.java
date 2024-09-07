234625226528de018214a2257fb4a2c789572a0f
Bruno Antico Galin <74074230+BrunoAG77@users.noreply.github.com>
2024-09-07 11:13:30 -0300

    Update TreeNodeOperator.java

--- TreeNodeOperator.java
+++ TreeNodeOperator.java
@@ -11,12 +11,12 @@ public class TreeNodeOperator extends TreeNode {
 	
 	private char operator;
 
-	public TreeNodeOperator(int key, TreeNode parent, char operator) {
+	public TreeNodeOperator(float key, TreeNode parent, char operator) {
 		super(key, parent);
 		this.operator = operator;
 	}
 	
-	public static boolean isOperator(char op) {
+	public boolean isOperator(char op) {
 		return (op == '+' || op == '-' || op == '*' || op == '/') ? true : false;
 	}
 
@@ -31,27 +31,29 @@ public class TreeNodeOperator extends TreeNode {
 		return 0;
 	}
 	
-    public float visitar() {   
-    	if (operator == '+'){
-    		return getRight().getKey() + getLeft().getKey();
-        }
-        if (operator == '-'){
-        	return getRight().getKey() - getLeft().getKey();
-        }
-        if (operator == '*'){
-        	return getRight().getKey() * getLeft().getKey();
-        }
-        if (operator == '/'){
-        	try{
-        		return getRight().getKey() / getLeft().getKey();
-            }
-            catch(ArithmeticException e){
-            	System.out.println("Erro: Divisão por zero.");
-            }
-        }
-        else{
-        	throw new IllegalArgumentException("Operador inválido: " + operator);
-        }
+    public float visitarOperador() {
+    	if (isOperator(operator)) {
+    		if (operator == '+'){
+    			return getRight().getKey() + getLeft().getKey();
+    		}
+    		if (operator == '-'){
+    			return getRight().getKey() - getLeft().getKey();
+    		}
+    		if (operator == '*'){
+    			return getRight().getKey() * getLeft().getKey();
+    		}
+    		if (operator == '/'){
+    			try{
+    				return getRight().getKey() / getLeft().getKey();
+    			}
+    			catch(ArithmeticException e){
+    				System.out.println("Erro: Divisão por zero.");
+    			}
+    		}
+    		else{
+    			throw new IllegalArgumentException("Operador inválido: " + operator);
+    		}
+    	}
         return 1;
     }
 
