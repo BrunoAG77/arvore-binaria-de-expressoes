ad7f276d72eb6ed73d1948c918359c9400d5394d
Bruno Antico Galin <74074230+BrunoAG77@users.noreply.github.com>
2024-09-18 23:00:49 -0300

    Create Multiplica.java

---
+++ Multiplica.java
@@ -0,0 +1,17 @@
+// Bruno Antico Galin 10417318
+// Ismael de Sousa e Silva 10410870
+// Referência: https://www.youtube.com/watch?v=Etpc_-br5rI
+// Referência: https://www.youtube.com/watch?v=b_NjndniOqY
+// Referência: https://www.youtube.com/watch?v=Gt2yBZAhsGM
+// Referência: https://www.youtube.com/watch?v=wL7JOLxbMI4
+package apl1_ed2;
+
+public class Multiplica extends TreeNodeOperator {
+	public Multiplica() {
+		super('*');
+	}
+	
+	public float multiplicar(float left, float right) {
+		return left * right;
+	}
+}
