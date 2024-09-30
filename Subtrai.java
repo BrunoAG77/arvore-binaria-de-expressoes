b43991aafa7bfc3b535cd92a613af21d2707131f
Bruno Antico Galin <74074230+BrunoAG77@users.noreply.github.com>
2024-09-29 22:34:40 -0300

    Update Subtrai.java

--- Subtrai.java
+++ Subtrai.java
@@ -4,14 +4,16 @@
 // Referência: https://www.youtube.com/watch?v=b_NjndniOqY
 // Referência: https://www.youtube.com/watch?v=Gt2yBZAhsGM
 // Referência: https://www.youtube.com/watch?v=wL7JOLxbMI4
-package apl1_ed2;
+package apl1_ed2copy;
 
 public class Subtrai extends TreeNodeOperator {
 	public Subtrai() {
 		super('-');
 	}
 	
-	public float subtrair(float left, float right) {
+	public float visitar() {
+		float left = getLeft().visitar();
+		float right = getRight().visitar();
 		return left - right;
 	}
 }
