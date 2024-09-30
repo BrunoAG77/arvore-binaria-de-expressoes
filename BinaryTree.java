07745da44bd2da0ed042409e58fb43859eb2a934
Bruno Antico Galin <74074230+BrunoAG77@users.noreply.github.com>
2024-09-29 22:35:52 -0300

    Update BinaryTree.java

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,96 +1,102 @@
-// Bruno Antico Galin 10417318
-// Ismael de Sousa e Silva 10410870
-// Referência: https://www.youtube.com/watch?v=Etpc_-br5rI
-// Referência: https://www.youtube.com/watch?v=b_NjndniOqY
-// Referência: https://www.youtube.com/watch?v=Gt2yBZAhsGM
-// Referência: https://www.youtube.com/watch?v=wL7JOLxbMI4
-
-package apl1_ed2;
+package apl1_ed2copy;
 
 import java.util.Stack;
 
 public class BinaryTree {
-	private TreeNode root;
-	private TreeNode left;
-	private TreeNode right;
-	
-	public BinaryTree() {
-		left = right = null;
-	}
-	
-	public TreeNode getRoot() {
-		return root;
-	}
-	
-	public void setRoot(TreeNode root) {
-		this.root = root;
-	}
-	
-	public boolean isEmpty() {
-		return root == null;
-	}
-	
-	public boolean isOperator(String op) {
-		return (op == "+" || op == "-" || op == "*" || op == "/") ? true : false;
-	}
-	
-	public void createTree(Stack<String> posfix) {
-		if (!posfix.isEmpty()) {
-			root = createNode(posfix);
-  	  	}
-	 }
-	
-	private TreeNode createNode(Stack<String> posfix) {
-		String value = posfix.pop();
-		if (isOperator(value)) {
-			TreeNodeOperator opnode = new TreeNodeOperator(value.charAt(0));
-			opnode.setRight(createNode(posfix));
-			opnode.setLeft(createNode(posfix));
-			return opnode;
+
+		private TreeNode root;
+		private TreeNode left;
+		private TreeNode right;
+		
+		public BinaryTree() {
+			left = right = null;
 		}
-		else {
-			TreeNodeOperand numnode = new TreeNodeOperand(Float.parseFloat(value));
-			return numnode;
+		
+		public TreeNode getRoot() {
+			return root;
+		}
+		
+		public void setRoot(TreeNode root) {
+			this.root = root;
+		}
+		
+		public boolean isEmpty() {
+			return root == null;
+		}
+		
+		public boolean isOperator(String op) {
+			return (op == "+" || op == "-" || op == "*" || op == "/") ? true : false;
+		}
+		
+		public void createTree(Stack<String> posfix) {
+			if (!posfix.isEmpty()) {
+				root = createNode(posfix);
+	  	  	}
+		 }
+		
+		private TreeNode createNode(Stack<String> posfix) {
+			String value = posfix.pop();
+			if (isOperator(value)) {
+				TreeNodeOperator opnode = null;
+				if (value == "+") {
+					opnode = new Soma();
+				}
+				if (value == "-") {
+					opnode = new Subtrai();
+				}
+				if (value == "*") {
+					opnode = new Multiplica();
+				}
+				if (value == "/") {
+					opnode = new Divide();
+				}
+				opnode.setRight(createNode(posfix));
+				opnode.setLeft(createNode(posfix));
+				return opnode;
+			}
+			else {
+				TreeNodeOperand numnode = new TreeNodeOperand(Float.parseFloat(value));
+				return numnode;
+			}
+		}
+		
+		public void preOrder() {
+			preOrder(root);
+		}
+		
+		public void inOrder() {
+			inOrder(root);
+		}
+		
+		public void postOrder() {
+			postOrder(root);
+		}
+		
+		private void preOrder(TreeNode root) {
+			if (root != null) {
+				System.out.print(root.getKey() + " ");
+				preOrder(root.getLeft());
+				preOrder(root.getRight());
+			}
+		}
+		
+		private void inOrder(TreeNode root) {
+			if (root != null) {
+				inOrder(root.getLeft());
+				System.out.print(root.getKey() + " ");
+				inOrder(root.getRight());
+			}
+		}
+		
+		private void postOrder(TreeNode root) {
+			if (root != null) {
+				postOrder(root.getLeft());
+				postOrder(root.getRight());
+				System.out.print(root.getKey() + " ");
+			}
+		}
+		
+		public void calcular() {
+			System.out.print(root.visitar());
 		}
 	}
-	
-	public void preOrder() {
-		preOrder(root);
-	}
-	
-	public void inOrder() {
-		inOrder(root);
-	}
-	
-	public void postOrder() {
-		postOrder(root);
-	}
-	
-	private void preOrder(TreeNode root) {
-		if (root != null) {
-			System.out.print(root.getKey() + " ");
-			preOrder(root.getLeft());
-			preOrder(root.getRight());
-		}
-	}
-	
-	private void inOrder(TreeNode root) {
-		if (root != null) {
-			inOrder(root.getLeft());
-			System.out.print(root.getKey() + " ");
-			inOrder(root.getRight());
-		}
-	}
-	
-	private void postOrder(TreeNode root) {
-		if (root != null) {
-			postOrder(root.getLeft());
-			postOrder(root.getRight());
-			System.out.print(root.getKey() + " ");
-		}
-	}
-	
-	public void calcular() {
-		System.out.print(root.visitar());
-	}
-}
